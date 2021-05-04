471ed62b76a55a562ced5e03f3d7a3de34a637e6
= <husamsaleem707@gmail.com>
2021-05-04 10:20:47 -0700

    Fixed bug

--- Online Blackjack Server/Program.cs
+++ Online Blackjack Server/Program.cs
@@ -5,8 +5,6 @@
         static Server server;
         static async System.Threading.Tasks.Task Main(string[] args)
         {
-            Blackjack bl = new Blackjack();
-            bl.Start();
             server = new Server();
             await server.Start();
         }
